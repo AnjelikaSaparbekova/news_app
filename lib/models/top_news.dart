c9154b4ced5486a949d8320e75f2cff989afb656
AnjelikaSaparbekova <anjelika.saparbekova5@gmail.com>
2023-09-12 23:43:21 +0300

    first commit

---
+++ lib/models/top_news.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:news_app/models/articles.dart';
+
+class TopNews {
+  const TopNews({
+    required this.status,
+    required this.totalResults,
+    required this.articles,
+  });
+
+  final String status;
+  final int totalResults;
+  final List<Articles> articles;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'status': status,
+      'totalResults': totalResults,
+      'articles': articles.map((x) => x.toJson()).toList(),
+    };
+  }
+
+  factory TopNews.fromJson(Map<String, dynamic> json) {
+    return TopNews(
+      status: json['status'] as String,
+      totalResults: json['totalResults'] as int,
+      articles: List<Articles>.from(
+        (json['articles'] as List<dynamic>).map<Articles>(
+          (x) => Articles.fromJson(x as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
+}
